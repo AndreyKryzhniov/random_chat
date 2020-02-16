5e0641993ffd0f35314a5726b9bb1d8ed97c081a
ai73aaa <1qazxcvBG>
2020-02-16 20:34:57 +0300

    add api: sendMessage, getMessages

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,11 +4,23 @@ const instance = axios.create({
     baseURL: "https://vast-sands-32724.herokuapp.com/",
 })
 
+type IMessage = {
+    message: string;
+    userId: number;
+    date: string;
+}
+
 export const api ={
     setUser () {
-        return instance.post('user')
+        return instance.post<{userId: number, status: string}>('user')
     },
     getUser (userId: number) {
-        return instance.get(`user?userId=${userId}`)
-    }
+        return instance.get<{chatId: number, status: string}>(`user?userId=${userId}`)
+    },
+    getMessages (userId: number, chatId: number, lastMessageDate: string) {
+        return instance.get<{messages: IMessage[], status: string}>(`message?userId=${userId}&chatId=${chatId}&date=${lastMessageDate}`)
+    },
+    sendMessage (userId: number, chatId: number, message: string) {
+        return instance.post<{status: string}>('message', {userId, chatId, message})
+    },
 }
