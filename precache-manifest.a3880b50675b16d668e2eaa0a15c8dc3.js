2e543ee864b0e0fa18911eefa93d54ce5bd11151
AndreyKryzhniov <andreykryzhniov@gmail.com>
2020-02-16 19:21:10 +0300

    Updates

---
+++ precache-manifest.a3880b50675b16d668e2eaa0a15c8dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedde737119975b99f4d669893f58bb2",
+    "url": "/random_chat/index.html"
+  },
+  {
+    "revision": "de6d1e85ef2d68c40d6e",
+    "url": "/random_chat/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef8475298972baed2a31",
+    "url": "/random_chat/static/js/2.2e1e7260.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/random_chat/static/js/2.2e1e7260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6d1e85ef2d68c40d6e",
+    "url": "/random_chat/static/js/main.5d13627a.chunk.js"
+  },
+  {
+    "revision": "9bea1bdbfa94974a99c7",
+    "url": "/random_chat/static/js/runtime-main.47209dfc.js"
+  }
+]);
