b2ef9eaa3c9660963296cf0a0259a8a6cc0e3a30
AndreyKryzhniov <andreykryzhniov@gmail.com>
2020-02-18 20:00:07 +0300

    Deleted debugger.

--- src/api/api.ts
+++ src/api/api.ts
@@ -21,7 +21,6 @@ export const api ={
         return instance.get<{messages: IMessage[], status: string}>(`message?userId=${userId}&chatId=${chatId}&date=${lastMessageDate}`)
     },
     sendMessage (userId: number, chatId: number, message: string) {
-        debugger
         return instance.post<{status: string}>('message', {userId, chatId, message})
     },
 }
